6a2d1467795a6fa8a607edb55d1cdeca0f466681
nkulka <45047069+nkulka@users.noreply.github.com>
2019-04-30 15:47:02 -0400

    usecase 3 basic implementation
    open html page and click on links. Only cse509 and blackboard is allowed rest wont go through.

--- Security Enhancing Browser Extension/contentscript.js
+++ Security Enhancing Browser Extension/contentscript.js
@@ -19,6 +19,39 @@ window.addEventListener("message", function(event) {
 })*/
 $(function() {
 var loginPage = false;
+
+/* new */
+var lstorage = window.localStorage
+lstorage.setItem("https://www.securitee.org", "1")
+lstorage.setItem("https://blackboard.stonybrook.edu", "2")
+
+var test = lstorage.getItem("https://blackboard.stonybrook.edu")
+console.log(test)
+
+var test1 = lstorage.getItem("lllall")
+console.log(test1)
+
+console.log("hiii hello")
+var x = document.links.length;
+console.log(x)
+
+var x = document.links[0].href;
+console.log(x)
+
+$('a').click( function(e) {/*e.preventDefault();*/
+  console.log("Url clicked")
+  var link = $(this).attr("href")
+  console.log(link)
+  
+  if(lstorage.getItem(link) == null) {
+    e.preventDefault();
+
+    /* code for popup/alert */
+  }
+
+  return true; } );
+/*new end*/
+
 var ip = document.getElementsByTagName("input");
 for (i = 0; i < ip.length; i++) {
   if(ip[i].getAttribute("type")=="password")
@@ -42,9 +75,9 @@ if(request.todo="checked")
 }
 })
 }*/
-document.getElementById("Password").onkeyup = function() {
+/*document.getElementById("Password").onkeyup = function() {
 myFunction()};
-
+*/
 function myFunction(str1) {
   //alert(document.getElementById("Password").value);
   //var res = str1.concat(document.getElementById("Password").value);
