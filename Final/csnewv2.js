7d78f8e8ca483dfb1edcdff8584107e3a69a61ca
avaradarajan <avaradarajan@cs.stonybrook.edu>
2019-05-04 00:57:39 -0400

    Final version 3.3

--- Final/csnewv2.js
+++ Final/csnewv2.js
@@ -7,6 +7,71 @@ var hashedPassword = null
 var lStorage = window.localStorage
 var salt = null
 
+$('a').contextmenu(function(e) {
+  console.log("Url clicked")
+  var link = $(this).attr("href")
+  e.preventDefault();
+  /* extract domain and do modifications according to DB */
+
+  console.log(link)
+  var hostname = (new URL(link)).hostname;
+  console.log(hostname)
+
+  /* if hostname contains www, remove it */
+
+  var searchItem = hostname.replace("www.", "")
+  console.log("Search item is")
+  console.log(searchItem)
+  chrome.runtime.sendMessage({todo : "searchDB", val : searchItem},
+    function (response) {
+      console.log(response.return)
+      if(response.return=="null"){
+
+
+      console.log("in if cs")
+
+      console.log("not allowed")
+      swal({
+        title:"Are you sure you want to do this?",
+        text:"We recommend not to proceed.......",
+        icon: "warning",
+        buttons:{
+          OK:true,
+          always: {
+            text:"Always for this site",
+            value:"always",
+          },
+          cancel:"Cancel",
+        },
+      })
+      .then((value) => {
+        switch(value) {
+          case "OK":
+          //console.log("OK")
+          window.open(link);
+          break;
+
+          case "always":
+          //console.log("always")
+          chrome.runtime.sendMessage({todo : "remember", val : searchItem})
+          window.open(link);
+          break;
+
+          default:
+          //console.log("cancel")
+        }
+      });
+    }
+    else if(response.return=="proceed")
+    {
+      // alert("in else in contextmenu")
+      // window.location.href = link
+      //$(link).contextmenu();
+    }
+    })
+
+});
+
 $('a').click(function(e) {
   console.log("Url clicked")
   var link = $(this).attr("href")
@@ -47,11 +112,20 @@ $('a').click(function(e) {
         switch(value) {
           case "OK":
           //console.log("OK")
+          if(e.ctrlKey){
+            window.open(link);
+            break;
+          }
           window.location.href = link
           break;
 
           case "always":
           //console.log("always")
+          if(e.ctrlKey){
+            chrome.runtime.sendMessage({todo : "remember", val : searchItem})
+            window.open(link);
+            break;
+          }
           chrome.runtime.sendMessage({todo : "remember", val : searchItem})
           window.location.href = link
           break;
@@ -69,7 +143,6 @@ $('a').click(function(e) {
     })
 
 });
-
 $(function() {
 domainName = document.domain //get current domain name
 
